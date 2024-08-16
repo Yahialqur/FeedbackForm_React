bdfc1235e524c20ff49339448b22eb266cb2133f
yahialqur <ymq2003@gmail.com>
2024-08-16 03:35:49 -0400

    Added submit functionalities

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -3,6 +3,39 @@ import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
 
+    const[formData, setFormData] = useState({
+        name: '',
+        email: '',
+        feedback: ''
+    });
+
+    const handleChange = (event) => {
+        const{name, value} = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const confirmationMessage = `
+            Name: ${formData.name}
+            Email: ${formData.email}
+            Feedback: ${formData.feedback}
+        `;
+        const isConfirmed = window.confirm(`Please confirm your details:\n\n${confirmationMessage}`);
+        if(isConfirmed){
+            console.log('Submitting feedback:', formData);
+            setFormData({
+                name: '',
+                email: '',
+                feedback: ''
+              });
+              alert('Thank you for your valuable feedback!');
+        }
+    };
+
   return (
     <>
     <nav>
@@ -16,15 +49,21 @@ const FeedbackForm = () => {
             type='text'
             name='name'
             placeholder='Your Name'
+            value={formData.name}
+            onChange={handleChange}
         />
         <input
             type='email'
             name='email'
             placeholder='Your Email'
+            value={formData.email}
+            onChange={handleChange}
         />
         <textarea
             name='feeback'
             placeholder='Your Feedback'
+            value={formData.feedback}
+            onChange={handleChange}
         ></textarea>
 
         <button type='submit'>Submit Feedback</button>
